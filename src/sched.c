ab82796d992ef135146ee858951024506492393c
Mèo Bách Khoa <truongtaman2004@gmail.com>
2024-05-25 17:32:52 +0700

    Update sched.c

--- src/sched.c
+++ src/sched.c
@@ -1,4 +1,5 @@
 
+#include "os-cfg.h"
 #include "queue.h"
 #include "sched.h"
 #include <pthread.h>
@@ -44,10 +45,26 @@ void init_scheduler(void) {
  */
 struct pcb_t * get_mlq_proc(void) {
 	struct pcb_t * proc = NULL;
-	/*TODO: get a process from PRIORITY [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 */
-	proc = dequeue(&mlq_ready_queue[0]);
+    pthread_mutex_lock(&queue_lock);
+    for (int i=0;i < MAX_PRIO;++i) {
+        if (!empty(&mlq_ready_queue[i]) && mlq_ready_queue[i].slot > 0) {
+            --mlq_ready_queue[i].slot;
+            proc = dequeue(&mlq_ready_queue[i]);
+            break;
+        }
+    }
+    pthread_mutex_unlock(&queue_lock);
+    if (proc == NULL) {
+        pthread_mutex_lock(&queue_lock);
+        for (int i=0;i < MAX_PRIO;++i) mlq_ready_queue[i].slot = MAX_PRIO - i;
+        for (int i=0;i < MAX_PRIO;++i)
+            if (!empty(&mlq_ready_queue[i])) {
+                --mlq_ready_queue[i].slot;
+                proc = dequeue(&mlq_ready_queue[i]);
+                break;
+            }
+        pthread_mutex_unlock(&queue_lock);
+    }
 	return proc;	
 }
 
@@ -77,9 +94,9 @@ void add_proc(struct pcb_t * proc) {
 #else
 struct pcb_t * get_proc(void) {
 	struct pcb_t * proc = NULL;
-	/*TODO: get a process from [ready_queue].
-	 * Remember to use lock to protect the queue.
-	 * */
+    pthread_mutex_lock(&queue_lock);
+    proc = dequeue(&ready_queue);
+    pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
