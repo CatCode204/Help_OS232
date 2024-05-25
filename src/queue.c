7cf2c5fa5aa3a5ab7fc236c5aa86c21934b7c07f
Minhdev04 <vuminh123@LAPTOP-ILNK3761.localdomain>
2024-05-25 17:07:00 +0700

    sched

--- src/queue.c
+++ src/queue.c
@@ -2,19 +2,44 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-        if (q == NULL) return 1;
+int empty(struct queue_t *q)
+{
+	if (q == NULL)
+		return 1;
 	return (q->size == 0);
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
-        /* TODO: put a new process to queue [q] */
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+	/* TODO: put a new process to queue [q] */
+	if (q->size >= MAX_QUEUE_SIZE)
+	{
+		return;
+	}
+	q->proc[q->size++] = proc;
 }
 
-struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
+struct pcb_t *dequeue(struct queue_t *q)
+{
+	/* TODO: return a pcb whose prioprity is the highest
+	 * in the queue [q] and remember to remove it from q
+	 * */
+	int i, highestPriority = 0;
+	if (q->size > 0)
+	{
+		for (i = 1; i < q->size; i++)
+		{
+			// printf("PRIO: %d\n", (int)q->proc[i]->prio);
+			if (q->proc[highestPriority]->prio > q->proc[i]->prio)
+			{
+				highestPriority = i;
+			}
+		}
+		struct pcb_t *tmp_proc = q->proc[highestPriority];
+		for (i = highestPriority; i < q->size - 1; i++)
+			q->proc[i] = q->proc[i + 1];
+		q->size--;
+		return tmp_proc;
+	}
 	return NULL;
 }
-
